3baa3bcd6b0d7ed596b2e15006ae175040626353
Peng Du <rick.peng.du@gmail.com>
2025-09-12 20:47:00 -0700

    add memory example

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-int main() {
-    // baby steps for C++
-    // array
-    int array[] = {1, 2, 3, 4, 5};
 
-    for (int i = 0; i < 5; ++i) {
-        std::cout << i << " : " << array[i] << std::endl;
+// Finds the element-wise maximum between two arrays of length n
+int *maxArrays(int *arr1, int *arr2, int n) {
+    int *result = new int[n];   // allocate n integers from heap
+    int *ptr = result;
+    for (int i = 0; i < n; ++i) {
+        *ptr = (*arr1 > *arr2) ? *arr1 : *arr2;
+        arr1++;
+        arr2++;
+        ptr++;
     }
+    return result;
+}
 
+// reference is only syntax sugar to pointer
 
+int main() {
+    int a[] = {1, 7, 3, 9, 5};
+    int b[] = {2, 6, 4, 8, 10};
+    int *result;
 
-    return 0;
+    result = maxArrays(a, b, 5);
+
+    std::cout << "Element-wise max array: ";
+    for (int i = 0; i < 5; ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << "\n";
 }
