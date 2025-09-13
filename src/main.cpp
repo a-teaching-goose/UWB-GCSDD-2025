ea4e33820d71eea54ee676a8c376d73184cdc5a7
Peng Du <rick.peng.du@gmail.com>
2025-09-12 19:37:03 -0700

    demoing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello C++ BootUp!" << std::endl;
+    // baby steps for C++
+    // array
+    int array[] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < 5; ++i) {
+        std::cout << i << " : " << array[i] << std::endl;
+    }
+
+
+
     return 0;
 }
